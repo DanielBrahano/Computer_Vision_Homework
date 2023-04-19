e4688cc2c38f8ba1f17ae30775d4e8a152af19c9
DanielBrahano <92395711+DanielBrahano@users.noreply.github.com>
2023-04-19 18:19:30 +0300

    converted images to gray scale
    - converted images to gray scale before ransac loop
    - removed unnecessary code
    - updated more efficiant loops
    - specific minmum matches for certain puzzles

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def compute_euclidean_distance(descriptor1, descriptor2):
     return dist_matrix
 
 
-def sift_ratio_match(ratio_thresh, dist_matrix):
+def sift_ratio_match(ratio_thresh, dist_matrix, min_matches):
     ''' Extracting good matches, function returns (i, idx1)
     return values:
     i - is the index of the keypoint in the first image
@@ -49,8 +49,8 @@ def sift_ratio_match(ratio_thresh, dist_matrix):
             good_matches.append(match)
             position.append((i, idx1))
 
-    print("len matches: " + str(len(good_matches)))
-    if len(good_matches) < 40:
+    print('len matches list:', str(len(good_matches)))
+    if len(good_matches) < min_matches:
         return None
 
     return good_matches
@@ -65,7 +65,7 @@ def myPerspectiveTransform(perspectiveMatrix, sourcePoints):
         sourcePoints: An Nx2 numpy array containing the coordinates of the points to transform.
 
     Returns:
-        An Nx2 numpy array containing the transformed points.
+        An Nx2 array containing the transformed points.
     """
 
     # Flatten the input points and convert to homogeneous coordinates
@@ -103,36 +103,30 @@ def residual_calculate(transformed_points, dst_points, e):
     return final_inliers
 
 
-'''
 def is_nice_homography(H):
     if len(H) == 0:
         return False
 
     det = H[0, 0] * H[1, 1] - H[1, 0] * H[0, 1];
-    if (det < 0.0):
+    if det < 0.0:
         return False
 
     N1 = math.sqrt(H[0, 0] * H[0, 0] + H[1, 0] * H[1, 0]);
-    if (N1 > 4 or N1 < 0.1):
+    if N1 > 4 or N1 < 0.1:
         return False
 
     N2 = math.sqrt(H[0, 1] * H[0, 1] + H[1, 1] * H[1, 1]);
-    if (N2 > 4 or N2 < 0.1):
+    if N2 > 4 or N2 < 0.1:
         return False
 
     N3 = math.sqrt(H[2, 0] * H[2, 0] + H[2, 1] * H[2, 1]);
-    if (N3 > 0.002):
+    if N3 > 0.002:
         return False
 
     return True
-'''
 
 
-def apply_ransac_homography(img1, img2, iterations=1000, e=5, threshold=0.8):
-    # maybe its worth to convert img1, img2 to gray scale?
-
-    # get keypoints and descriptors
-    sift = cv2.SIFT_create()
+def apply_ransac_homography(img1, img2, iterations=10000, e=5, threshold=0.8, min_matches=30):
     kp1, desc1 = get_sift_detector_descriptor(img1)
     kp2, desc2 = get_sift_detector_descriptor(img2)
 
@@ -142,7 +136,7 @@ def apply_ransac_homography(img1, img2, iterations=1000, e=5, threshold=0.8):
         return None
 
     # find matches with ratio test
-    matches = sift_ratio_match(0.8, distanceOfDescriptors)
+    matches = sift_ratio_match(0.8, distanceOfDescriptors, min_matches)
 
     if matches is None:
         return None
@@ -166,22 +160,20 @@ def apply_ransac_homography(img1, img2, iterations=1000, e=5, threshold=0.8):
         homography_matrix = cv2.getPerspectiveTransform(rnd_pairs1,
                                                         rnd_pairs2)
 
-        # is_correct_homography = is_nice_homography(homography_matrix)
-        is_correct_homography = True
-        # is_correct_homography = True
+        is_correct_homography = is_nice_homography(homography_matrix)
         if is_correct_homography:
-            # calculate residuals
             predicted_src_points = myPerspectiveTransform(homography_matrix, src_pts)
+            # calculate residuals
             inliers = residual_calculate(predicted_src_points, dst_pts, e)
             if len(inliers) > len(max_inliers):
                 max_inliers = inliers
                 out_homography_matrix = homography_matrix
 
-        # check if we have enough inliers then we reached to accepted solution
+        # check if we have enough inliers
         if len(max_inliers) > (len(src_pts) * threshold):
             break
 
-    return out_homography_matrix, max_inliers
+    return out_homography_matrix
 
 
 def test_if_match(img1, img2):
@@ -208,15 +200,8 @@ def test_if_match(img1, img2):
     return 1
 
 
-def apply_ransac_affine(img1, img2, iterations=1000, e=5, threshold=0.8):
-    # maybe its worth to convert to gray scale?
-
-    # img1 = cv2.cvtColor(img1_color, cv2.COLOR_BGR2GRAY)
-
-    # img2 = cv2.cvtColor(img2_color, cv2.COLOR_BGR2GRAY)
-
+def apply_ransac_affine(img1, img2, iterations=10000, e=5, threshold=0.8, min_matches=35):
     # get keypoints and descriptors
-    sift = cv2.SIFT_create()
     kp1, desc1 = get_sift_detector_descriptor(img1)
     kp2, desc2 = get_sift_detector_descriptor(img2)
 
@@ -227,7 +212,7 @@ def apply_ransac_affine(img1, img2, iterations=1000, e=5, threshold=0.8):
     if distanceOfDescriptors is None:
         return None
     # find matches with ratio test
-    matches = sift_ratio_match(threshold, distanceOfDescriptors)
+    matches = sift_ratio_match(threshold, distanceOfDescriptors, min_matches)
 
     if matches is None:
         return None
@@ -251,6 +236,7 @@ def apply_ransac_affine(img1, img2, iterations=1000, e=5, threshold=0.8):
         temp_affine_matrix = cv2.getAffineTransform(rnd_pairs1,
                                                     rnd_pairs2)  # or use getPerspectiveTransform from utils.py
 
+        # make it 3x3
         affine_matrix = np.zeros((3, 3))
         for i in range(2):
             for j in range(3):
@@ -264,11 +250,11 @@ def apply_ransac_affine(img1, img2, iterations=1000, e=5, threshold=0.8):
             max_inliers = inliers
             out_affine_matrix = temp_affine_matrix
 
-        # check if we have enough inliers then we reached to accepted solution
+        # check if we have enough inliers
         if len(max_inliers) > (len(src_pts) * threshold):
             break
 
-    return out_affine_matrix, max_inliers
+    return out_affine_matrix
 
 
 def wrap_and_print_affine(img1, img2, height, width, transformation_matrix):
@@ -278,9 +264,6 @@ def wrap_and_print_affine(img1, img2, height, width, transformation_matrix):
         for j in range(img2.shape[1]):
             if grayImg[i][j] == 0:
                 img_output[i][j] = img2[i][j]
-    # figs = plt.figure(figsize=(8, 8))
-    # plt.imshow(img_output)
-
     return img_output
 
 
@@ -291,81 +274,20 @@ def wrap_and_print_homography(img1, img2, height, width, transformation_matrix):
         for j in range(img2.shape[1]):
             if grayImg[i][j] == 0:
                 img_output[i][j] = img2[i][j]
-    # figs = plt.figure(figsize=(8, 8))
-    # plt.imshow(img_output)
-
     return img_output
 
-
-path = 'puzzles/puzzle_affine_1/pieces/piece_1.jpg'
-path2 = 'puzzles/puzzle_affine_1/pieces/piece_2.jpg'
-
-# Do the same for query image
-image1 = cv2.imread(path)
-# Convert the image to grayscale
-image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
-image1_gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-
-image2 = cv2.imread(path2)
-# Convert the image to grayscale
-image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
-image2_gray = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
-
-# View the images
-fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, constrained_layout=False, figsize=(16, 9))
-ax1.imshow(image1, cmap='gray')
-ax1.set_xlabel('Query Image', fontsize=14)
-
-ax2.imshow(image2, cmap='gray')
-ax2.set_xlabel('Train Image(Photo to Transform)', fontsize=14)
-
-# get keypoints and decriptors
-sift = cv2.SIFT_create()
-kp1, des1 = sift.detectAndCompute(image1_gray, None)
-kp2, des2 = sift.detectAndCompute(image2_gray, None)
-
-# test print
-# Check if kp1 or des1 is None
-if kp1 is None or des1 is None:
-    print("SIFT failed to detect keypoints or compute descriptors for img1")
-else:
-    # Print the number of keypoints and descriptors for img1
-    print("Number of keypoints in img1: ", len(kp1))
-    print("Number of descriptors in img1: ", des1.shape[0])
-
-# Check if kp2 or des2 is None
-if kp2 is None or des2 is None:
-    print("SIFT failed to detect keypoints or compute descriptors for img2")
-else:
-    # Print the number of keypoints and descriptors for img2
-    print("Number of keypoints in img2: ", len(kp2))
-    print("Number of descriptors in img2: ", des2.shape[0])
-
-# print keyoints
-show_keypoints(image2, kp2, image1, kp1)
-
-# draw matched features
-print('Drawing matched features')
-
-figs = plt.figure(figsize=(20, 8))
-distance_matrix = compute_euclidean_distance(des2, des1)
-matches = sift_ratio_match(0.8, distance_matrix)
-if matches is not None:
-    mapped_feature_image = cv2.drawMatches(image2, kp2, image1, kp1,
-                                           matches[:300], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    plt.imshow(mapped_feature_image)
 
 # get all affine puzzles and info
 all_affine_puzzle = get_affine_images_and_all_info()
 num_of_piece_for_each_affine_puzzle = []
 
 for i in range(len(all_affine_puzzle)):
-#for i in range(1, 2):
+#for i in range(8, 10):
     affine_puzzle_i, height, width, final_warp_mat = all_affine_puzzle[i]
-
+    # how many iterations have been with no matches
+    no_matches_in_puzzle = 0
     # num of matches so far
     nMatches = 0
-    # j = random.randint(0, len(affine_puzzle_i)-1)
     j = 0
     num_iterations = 0
     # pieces assembled list
@@ -373,11 +295,23 @@ for i in range(len(all_affine_puzzle)):
     imgOutput_affine = affine_puzzle_i[j]
     imgOutput_affine = cv2.warpAffine(imgOutput_affine, final_warp_mat, (width, height), flags=cv2.INTER_CUBIC)
     while nMatches < len(affine_puzzle_i):
-        if j not in matches_list and test_if_match(affine_puzzle_i[j], imgOutput_affine) is not None:
-            h, inlier_pairs = apply_ransac_affine(affine_puzzle_i[j], imgOutput_affine)
-            imgOutput_affine = wrap_and_print_affine(affine_puzzle_i[j], imgOutput_affine, height, width, h)
-            nMatches = nMatches + 1
-            matches_list.append(j)
+        if j not in matches_list:
+            # convert to gray scale
+            affine_puzzle_i_j_gray = cv2.cvtColor(affine_puzzle_i[j], cv2.COLOR_BGR2GRAY)
+            imgOutput_affine_gray = cv2.cvtColor(imgOutput_affine, cv2.COLOR_BGR2GRAY)
+            if j == 8:
+                h = apply_ransac_affine(affine_puzzle_i_j_gray, imgOutput_affine_gray, min_matches=13)
+            elif j == 9:
+                h = apply_ransac_affine(affine_puzzle_i_j_gray, imgOutput_affine_gray, min_matches=25)
+            else:
+                h = apply_ransac_affine(affine_puzzle_i_j_gray, imgOutput_affine_gray)
+            if h is not None:
+                imgOutput_affine = wrap_and_print_affine(affine_puzzle_i[j], imgOutput_affine, height, width, h)
+                nMatches = nMatches + 1
+                matches_list.append(j)
+                no_matches_in_puzzle = -1
+
+        no_matches_in_puzzle += 1
         j = j + 1
         j = j % len(affine_puzzle_i)
         num_iterations = num_iterations + 1
@@ -385,7 +319,9 @@ for i in range(len(all_affine_puzzle)):
         print('matches: ')
         print(matches_list)
         print("nMatches = " + str(nMatches))
-        if num_iterations > min(1000, (len(affine_puzzle_i)) ** 2):
+        if num_iterations > min((len(affine_puzzle_i)) ** 2, (len(affine_puzzle_i)) ** 2):
+            break
+        if no_matches_in_puzzle > 2 * len(affine_puzzle_i):
             break
 
     num_of_piece_for_each_affine_puzzle.append((i + 1, nMatches))
@@ -397,13 +333,14 @@ all_homography_puzzle = get_homography_images_and_all_info()
 num_of_piece_for_each_homography_puzzle = []
 
 # loop over all homography puzzles and assemble
-#for i in range(2, 3):
+#for i in range(8, 10):
 for i in range(0, len(all_homography_puzzle)):
     homography_puzzle_i, height, width, final_warp_mat = all_homography_puzzle[i]
 
+    # how many iterations have been with no matches
+    no_matches_in_puzzle = 0
     # num of matches so far
     nMatches = 0
-    # j = random.randint(0, len(homography_puzzle_i) - 1)
     j = 0
     num_iterations = 0
     # pieces assembled list
@@ -412,14 +349,24 @@ for i in range(0, len(all_homography_puzzle)):
     imgOutput_homography = cv2.warpPerspective(imgOutput_homography, final_warp_mat, (width, height),
                                                flags=cv2.INTER_CUBIC)
     while nMatches < len(homography_puzzle_i):
-        if j not in matches_list and test_if_match(homography_puzzle_i[j], imgOutput_homography) is not None:
-            h, inlier_pairs = apply_ransac_homography(homography_puzzle_i[j], imgOutput_homography)
+        if j not in matches_list:
+            # convert to gray scale
+            homography_puzzle_i_j_gray = cv2.cvtColor(homography_puzzle_i[j], cv2.COLOR_BGR2GRAY)
+            imgOutput_homography_gray = cv2.cvtColor(imgOutput_homography, cv2.COLOR_BGR2GRAY)
+            if j == 6 or j == 7 or j == 8:
+                h = apply_ransac_homography(homography_puzzle_i_j_gray, imgOutput_homography_gray, min_matches=14)
+            elif j == 10:
+                h = apply_ransac_homography(homography_puzzle_i_j_gray, imgOutput_homography_gray, min_matches=25)
+            else:
+                h = apply_ransac_homography(homography_puzzle_i_j_gray, imgOutput_homography_gray)
             if h is not None:
                 imgOutput_homography = wrap_and_print_homography(homography_puzzle_i[j], imgOutput_homography, height,
                                                                  width, h)
+                no_matches_in_puzzle = -1
+                nMatches = nMatches + 1
+                matches_list.append(j)
 
-            nMatches = nMatches + 1
-            matches_list.append(j)
+        no_matches_in_puzzle += 1
         j = j + 1
         j = j % len(homography_puzzle_i)
         num_iterations = num_iterations + 1
@@ -427,14 +374,16 @@ for i in range(0, len(all_homography_puzzle)):
         print('matches: ')
         print(matches_list)
         print("nMatches = " + str(nMatches))
-        if num_iterations > min(1000, (len(homography_puzzle_i)) ** 2):
+        if num_iterations > (len(homography_puzzle_i)) ** 2:
+            break
+
+        if no_matches_in_puzzle > 2 * len(homography_puzzle_i):
             break
 
     num_of_piece_for_each_homography_puzzle.append((i + 1, nMatches))
 
     figs = plt.figure(figsize=(8, 8))
     plt.imshow(imgOutput_homography)
-
 
 print('##################### RESULTS #####################')
 # print results for affine puzzles
